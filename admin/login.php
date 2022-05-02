7cf6c856ad609bf32010206da87e1a85913df5f4
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2022-05-02 22:58:34 +0700

    Update login admin

--- admin/login.php
+++ admin/login.php
@@ -1,15 +1,16 @@
 <?php
-	include '../classses/adminlogin.php';
-	$class = new adminlogin();
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$adminUser = $_POST['adminUser'];
-		$adminPass = md5($_POST['adminPass']);
+include '../classses/adminlogin.php';
+$class = new adminlogin();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $adminUser = $_POST['adminUser'];
+    $adminPass = md5($_POST['adminPass']);
 
-		$login_check = $class->login_admin($adminUser,$adminPass);
-	}
+    $login_check = $class->login_admin($adminUser, $adminPass);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,49 +20,32 @@
     <link rel="stylesheet" href="assets/font/themify-icons/themify-icons.css">
     <title>Đăng nhập admin</title>
 </head>
+
 <body>
-    <div class="modal">
-        <div class="modal__overlay"></div>
-        <div class="modal__body">  
-               <!-- Login form-->
-            <form action="login.php" method="post" >
-               <div class="auth__form">
-                    <div class="auth__form-container">
-                        <span class="ti-github"></span>
-                        <div class="auth__form-header">
-                            <h4 class="auth__form-heading">Đăng nhập</h4>
-                        </div>
-                        <div class="auth__form-form">
-                            <div class="auth__form-group">
-                                <input type="text" class="auth__form-input"  name="adminUser" >
-                            </div>
-                            <div class="auth__form-group">
-                                <input type="password" class="auth__form-input"  name="adminPass" >
-                            </div>
-                        </div>
-                        <div class="auth__form-aside">
-                            <div class="auth__form-help">
-                                <a href="" class="auth__form-help-link">Quên mật khẩu</a>
-                                <span class="auth__form-help-separate"></span>
-                                <div class="auth__form-aside__link">
-                                    <a href="" class="auth__form-help-link">Cần trợ giúp ?</a>
-                                </div>    
-                            </div>
-                        </div>
-                        <div class="login-btn" style=" color: #fff; text-align: center; margin: 20px;">
-                            <?php
-                            if (isset($login_check)) {
-                                echo $login_check;
-                            }
-                            ?>
-                        </div>
-                        <div class="auth__form-controls">
-                            <input type="submit" value="Đăng nhập" class="btn btn-login mt-16">
-                        </div>
-                    </div>
-                </div>
-            </form>
+    <!-- partial:index.partial.html -->
+    <form action="login.php" method="post" class="login-form">
+        <p class="login-text">
+            <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-lock fa-stack-1x"></i>
+            </span>
+        </p>
+        <input type="text" name="adminUser" class="login-username" autofocus="true" required="true" placeholder="Tên đăng nhâp" />
+        <input type="password" name="adminPass" class="login-password" required="true" placeholder="Password" />
+        <div class="login-form-waring" >
+            <?php
+            if (isset($login_check)) {
+                echo $login_check;
+            }
+            ?>
         </div>
-    </div>
+        <input type="submit" name="Login" value="Login" class="login-submit" />
+
+    </form>
+    <a href="#" class="login-forgot-pass">forgot password?</a>
+    <div class="underlay-photo"></div>
+    <div class="underlay-black"></div>
+    <!-- partial -->
 </body>
+
 </html>
