65c6a39fea113b56c0db11964ad9e421f888bcf1
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2023-02-01 22:07:14 +0700

    Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Fashion product trading system
+# Fashion product sale website
 
-# Team  Members:
+## Team  Members:
 ```bash
     Saigon University ,Faculty of Information Technology ,Major in Software Engineering
     ID          | Full Name
@@ -8,3 +8,41 @@
     3120410316    Quan  Van Manh
     3120410300    Doan Thanh Loi
 ```
+## Project Description:
+This is the website that the team did during the learning process, this is the age of technology, so users mainly shop through websites.
+Seizing that opportunity, the team built a website to help customers access the store on a small scale by buying goods through online clearance.
+## Tech Stack Used:
+```bash
+    FrontEnd:
+        * Javascript
+        * CSS
+        * HTML
+    BackEnd:
+        * PHP
+    Database:
+        * MySql
+```
+## Website Functions:
+For users:
+* **Login / registration function
+* **Function to add products to cart
+* **Product search/category function
+* **User function can view/edit personal information
+* **User function can manage orders (pending / confirmed / shipped / canceled) when admin has accepted
+* **User password change function
+For admins:
+* **Admin can add/edit/delete products
+* **Admin can manage orders (confirm orders the user has placed)
+* **Admin can statistic the business situation of the store by month / quarter / year
+* **Admin can manage registered customer information
+* **Admin can build promotional vouchers for new users or major holidays
+## Steps to run the website:
+1. Clone from github and add to htdocs folder in xampp
+2. Launch the application and go to the directory config -> config.php , change the names according to your Mysql
+3. Add doanweb.sql in Database folder to Mysql
+4. Then launch the website
+## [Project Demo Link](https://youtu.be/6VLaAa8GNDc)
+## Some features of the site:
+Home Page                   |                   Registration page
+:---------------------------------:        |      :------------------------------:
+<img src="Screenshots/HomePage.png" height="200">  | <img src="Screenshots/RegistrtionPage.png" height="200">
--- assets/js/donhang.js
+++ assets/js/donhang.js
@@ -4,148 +4,146 @@ let table = document.getElementById('choXN');
 btnQL[0].style.background = "#2a2b2c";
 btnQL[0].style.color = "white";
 
-let table_0=document.getElementsByClassName('table_0');
-let table_1=document.getElementsByClassName('table_1');
-let table_2=document.getElementsByClassName('table_2');
-let table_3=document.getElementsByClassName('table_3');
-let table_4=document.getElementsByClassName('table_4');
+let table_0 = document.getElementsByClassName('table_0');
+let table_1 = document.getElementsByClassName('table_1');
+let table_2 = document.getElementsByClassName('table_2');
+let table_3 = document.getElementsByClassName('table_3');
+let table_4 = document.getElementsByClassName('table_4');
 
-let display_0=document.getElementsByClassName('display_0');
-let display_1=document.getElementsByClassName('display_1');
-let display_2=document.getElementsByClassName('display_2');
-let display_3=document.getElementsByClassName('display_3');
-let display_4=document.getElementsByClassName('display_4');
+let display_0 = document.getElementsByClassName('display_0');
+let display_1 = document.getElementsByClassName('display_1');
+let display_2 = document.getElementsByClassName('display_2');
+let display_3 = document.getElementsByClassName('display_3');
+let display_4 = document.getElementsByClassName('display_4');
 
 
 
-for(let i=0;i<table_0.length;i++)
+for (let i = 0; i < table_0.length; i++)
     table_0[i].style.display = 'block';
 
-for(let i=0;i<table_1.length;i++)
+for (let i = 0; i < table_1.length; i++)
     table_1[i].style.display = 'none';
 
-for(let i=0;i<table_2.length;i++)
+for (let i = 0; i < table_2.length; i++)
     table_2[i].style.display = 'none';
 
-for(let i=0;i<table_3.length;i++)
+for (let i = 0; i < table_3.length; i++)
     table_3[i].style.display = 'none';
 
-for(let i=0;i<table_4.length;i++)
+for (let i = 0; i < table_4.length; i++)
     table_4[i].style.display = 'none';
 
 
-btnQL[0].addEventListener('click',() =>{
-    for(let i=0;i<table_0.length;i++){
+btnQL[0].addEventListener('click', () => {
+    for (let i = 0; i < table_0.length; i++) {
         table_0[i].style.display = 'block';
         display_0[i].style.display = 'none';
     }
-            
-
-    for(let i=0;i<table_1.length;i++){
+    for (let i = 0; i < table_1.length; i++) {
         table_1[i].style.display = 'none';
         display_1[i].style.display = 'none';
     }
-            
-    for(let i=0;i<table_2.length;i++){
+
+    for (let i = 0; i < table_2.length; i++) {
         table_2[i].style.display = 'none';
         display_2[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_3.length;i++){
+    for (let i = 0; i < table_3.length; i++) {
         table_3[i].style.display = 'none';
         display_3[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_4.length;i++){
+    for (let i = 0; i < table_4.length; i++) {
         table_4[i].style.display = 'none';
         display_4[i].style.display = 'none';
     }
 })
 
-btnQL[1].addEventListener('click',() =>{
-    for(let i=0;i<table_1.length;i++){
+btnQL[1].addEventListener('click', () => {
+    for (let i = 0; i < table_1.length; i++) {
         table_1[i].style.display = 'block';
         display_1[i].style.display = 'none';
     }
-            
 
-    for(let i=0;i<table_0.length;i++){
+
+    for (let i = 0; i < table_0.length; i++) {
         table_0[i].style.display = 'none';
         display_0[i].style.display = 'none';
     }
-            
-    for(let i=0;i<table_2.length;i++){
+
+    for (let i = 0; i < table_2.length; i++) {
         table_2[i].style.display = 'none';
         display_2[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_3.length;i++){
+    for (let i = 0; i < table_3.length; i++) {
         table_3[i].style.display = 'none';
         display_3[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_4.length;i++){
+    for (let i = 0; i < table_4.length; i++) {
         table_4[i].style.display = 'none';
         display_4[i].style.display = 'none';
     }
 })
 
-btnQL[3].addEventListener('click',() =>{
-    for(let i=0;i<table_3.length;i++){
+btnQL[3].addEventListener('click', () => {
+    for (let i = 0; i < table_3.length; i++) {
         table_3[i].style.display = 'block';
         display_3[i].style.display = 'none';
     }
-            
 
-    for(let i=0;i<table_1.length;i++){
+
+    for (let i = 0; i < table_1.length; i++) {
         table_1[i].style.display = 'none';
         display_1[i].style.display = 'none';
     }
-            
-    for(let i=0;i<table_0.length;i++){
+
+    for (let i = 0; i < table_0.length; i++) {
         table_0[i].style.display = 'none';
         display_0[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_2.length;i++){
+    for (let i = 0; i < table_2.length; i++) {
         table_2[i].style.display = 'none';
         display_2[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_4.length;i++){
+    for (let i = 0; i < table_4.length; i++) {
         table_4[i].style.display = 'none';
         display_4[i].style.display = 'none';
     }
 })
 
-btnQL[2].addEventListener('click',() =>{
-    for(let i=0;i<table_2.length;i++){
+btnQL[2].addEventListener('click', () => {
+    for (let i = 0; i < table_2.length; i++) {
         table_2[i].style.display = 'block';
         display_2[i].style.display = 'none';
     }
-            
 
-    for(let i=0;i<table_1.length;i++){
+
+    for (let i = 0; i < table_1.length; i++) {
         table_1[i].style.display = 'none';
         display_1[i].style.display = 'none';
     }
-            
-    for(let i=0;i<table_0.length;i++){
+
+    for (let i = 0; i < table_0.length; i++) {
         table_0[i].style.display = 'none';
         display_0[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_3.length;i++){
+    for (let i = 0; i < table_3.length; i++) {
         table_3[i].style.display = 'none';
         display_3[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_4.length;i++){
+    for (let i = 0; i < table_4.length; i++) {
         table_4[i].style.display = 'none';
         display_4[i].style.display = 'none';
     }
 })
-    
+
 function changeProductList(type, element) {
     let tabs = document.getElementsByClassName('QLdonhang');
     for (i = 0; i < tabs.length; i++) {
@@ -154,51 +152,50 @@ function changeProductList(type, element) {
     }
     element.style.background = '#2a2b2c';
     element.style.color = 'white';
-    
+
     document.getElementById(type).style.display = 'block';
 
 }
 
-btnQL[4].addEventListener('click',() =>{
-    for(let i=0;i<table_4.length;i++){
+btnQL[4].addEventListener('click', () => {
+    for (let i = 0; i < table_4.length; i++) {
         table_4[i].style.display = 'block';
         display_4[i].style.display = 'none';
     }
-            
 
-    for(let i=0;i<table_1.length;i++){
+
+    for (let i = 0; i < table_1.length; i++) {
         table_1[i].style.display = 'none';
         display_1[i].style.display = 'none';
     }
-            
-    for(let i=0;i<table_0.length;i++){
+
+    for (let i = 0; i < table_0.length; i++) {
         table_0[i].style.display = 'none';
         display_0[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_3.length;i++){
+    for (let i = 0; i < table_3.length; i++) {
         table_3[i].style.display = 'none';
         display_3[i].style.display = 'none';
     }
 
-    for(let i=0;i<table_2.length;i++){
+    for (let i = 0; i < table_2.length; i++) {
         table_2[i].style.display = 'none';
         display_2[i].style.display = 'none';
     }
 })
 
 //hiển thi chi tiết tung đơn hàng
-let toggle=document.getElementsByClassName('toggle');
-let display_body=document.getElementsByClassName('display');
-for(let i=0;i<toggle.length;i++){
+let toggle = document.getElementsByClassName('toggle');
+let display_body = document.getElementsByClassName('display');
+for (let i = 0; i < toggle.length; i++) {
 
-    toggle[i].addEventListener('click',() =>{
-        if(display_body[i].style.display=="none"){
-            display_body[i].style.display="block"
+    toggle[i].addEventListener('click', () => {
+        if (display_body[i].style.display == "none") {
+            display_body[i].style.display = "block"
+        } else {
+            display_body[i].style.display = "none"
         }
-        else{
-            display_body[i].style.display="none"
-        }
-            
+
     })
 }
